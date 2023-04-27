a40ed95bff89dd73d849c4ceab5928e46e121d0b
singhayushh <coding.ayush@gmail.com>
2023-04-27 19:58:03 +0530

    Added: UI based on tabler

---
+++ static/js/custom.js
@@ -0,0 +1,43 @@
+$(function () {
+    Dropzone.autoDiscover = false;
+
+    const dropzones = []
+    $('.dropzone').each(function (i, el) {
+        const name = $(el).attr('id')
+        var myDropzone = new Dropzone(el, {
+            url: window.location.pathname,
+            autoProcessQueue: false,
+            uploadMultiple: false,
+            parallelUploads: 100,
+            maxFiles: 100,
+            paramName: name,
+            addRemoveLinks: true,
+        })
+        dropzones.push(myDropzone)
+    })
+
+
+    document.querySelector("button[type=submit]").addEventListener("click", function (e) {
+        // Make sure that the form isn't actually being sent.
+        e.preventDefault();
+        e.stopPropagation();
+        let form = new FormData($('form')[0])
+
+        dropzones.forEach(dropzone => {
+            let { paramName } = dropzone.options
+            dropzone.files.forEach((file, i) => {
+                form.append(paramName, file)
+            })
+        })
+        console.log(form);
+        $.ajax({
+            method: 'POST',
+            data: form,
+            processData: false,
+            contentType: false,
+            success: function (response) {
+                document.write(response)
+            }
+        });
+    });
+});
